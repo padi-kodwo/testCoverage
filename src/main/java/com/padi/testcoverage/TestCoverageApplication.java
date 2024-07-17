3390ebe403b41afff5734852665cde01f118396a
Padi Amu <padi.amu@sita.aero>
2024-07-17 15:01:00 +0100

    - clean up

--- src/main/java/com/padi/testcoverage/TestCoverageApplication.java
+++ src/main/java/com/padi/testcoverage/TestCoverageApplication.java
@@ -1,13 +1,57 @@
 package com.padi.testcoverage;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class TestCoverageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestCoverageApplication.class, args);
+		SpringApplication app = new SpringApplication(TestCoverageApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
 	}
 
+	private static void logApplicationStartup(final Environment env) {
+
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		String serverPort = env.getProperty("server.port");
+		String contextPath = env.getProperty("server.servlet.context-path");
+		if (StringUtils.isBlank(contextPath)) {
+			contextPath = "/";
+		}
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("The host name could not be determined, using `localhost` as fallback");
+		}
+
+		log.info("""
+            ----------------------------------------------------------
+            \tApplication '{}' is running! Access URLs:
+            \tLocal: \t\t{}://localhost:{}{}
+            \tExternal: \t{}://{}:{}{}
+            \tProfile(s): \t{}
+            ----------------------------------------------------------""",
+				env.getProperty("spring.application.name"),
+				protocol,
+				serverPort,
+				contextPath,
+				protocol,
+				hostAddress,
+				serverPort,
+				contextPath,
+				env.getActiveProfiles());
+	}
 }

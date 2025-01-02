1cdaeef754d33b36ac80b3bca5518b50905044ee
Padi Amu <padiamu@gmail.com>
2025-01-02 12:01:27 +0100

    -docker services updated

---
+++ docker/db_config.sql
@@ -0,0 +1,7 @@
+ALTER USER keycloak WITH LOGIN CREATEDB CREATEROLE;
+
+LOGIN: Allows the user to log in.
+CREATEDB: Allows the user to create databases.
+CREATEROLE: Allows the user to create roles.
+SUPERUSER: Grants superuser privileges.
+REPLICATION: Grants replication privileges.
